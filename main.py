4a9d15a5f6d3d98c81569418688401b4b99d0d7a
Fahad Ahmed <fahad838@gmail.com>
2025-02-11 23:24:21 +0530

    Print a report of char in text

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     text = get_book_text(book_path)
     num_words = count_words(text)
     num_characters = count_characters(text)
-    print(num_characters)
+    print_report(book_path,num_words,sort_dict(num_characters))
 
 def get_book_text(path):
     with open(path) as f:
@@ -22,6 +22,29 @@ def count_characters(string):
         else:
             char_dict[char] = 1
     return char_dict
+
+def sort_on(dict):
+    return dict["count"]
+
+def sort_dict(dict):
+    list_of_dict = [{"char":k, "count":v} for k,v in dict.items()]
+    list_of_dict.sort(reverse=True, key=sort_on)
+    return list_of_dict
+
+
+
+def print_report(path,num_words, char_dict_list):
+    print(f"--- Begin report of {path} ---")
+    print(f"{num_words} words found in the document")
+    print("")
+    for item in char_dict_list:
+        char = item["char"]
+        count = item["count"]
+        if char.isalpha():
+            print(f"The '{char}' character was found {count} times")
+    print("--- End report ---")
+        
+            
 
 
 
